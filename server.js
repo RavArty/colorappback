1f2982bd417e761d52197754b2ae61d92208f6cb
Ravshan <ravkart@gmail.com>
2019-07-08 22:02:29 -0700

    updated rank

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 const database = {
 	users: [
@@ -28,18 +32,20 @@ const database = {
 app.get('/', (req, res) => {
 	res.send(database.users)
 })
-
+//------------------------------------------------------------------------
 app.post('/signin', (req, res) => {
 	if (req.body.email === database.users[0].email &&
 			req.body.password === database.users[0].password ){
-		res.json('success')
+		res.json(database.users[0])
 	} else {
 		res.status(400).json('error logging in')
 	}
 })
-
+//------------------------------------------------------------------------
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body
+	const hash = bcrypt.hashSync(password, saltRounds)
+	console.log(hash)
 	database.users.push({
 		id: '125',
 		name: name,
@@ -50,7 +56,7 @@ app.post('/register', (req, res) => {
 	})
 	res.json(database.users[database.users.length - 1])
 })
-
+//------------------------------------------------------------------------
 app.get('/profile/:id', (req, res) => {
 	const {id} = req.params
 	let found = false
@@ -64,7 +70,7 @@ app.get('/profile/:id', (req, res) => {
 		res.status(400).json('not found')
 	}
 })
-
+//------------------------------------------------------------------------
 app.put('/image', (req, res) => {
 	const {id} = req.body
 	let found = false
@@ -79,7 +85,7 @@ app.put('/image', (req, res) => {
 		res.status(400).json('not found')
 	}
 })
-
+//------------------------------------------------------------------------
 app.listen(3000, () => {
 	console.log("app is running on port 3000")
 })
